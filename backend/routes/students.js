bd125bfd83431ef1c11fa6403bdd4f57afcfbd82
Binoy Anil <100678709+Bino369@users.noreply.github.com>
2025-11-19 22:39:21 +0530

    Update students.js

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,7 +1,7 @@
+import express from 'express';
+import Student from '../models/Student.js';
 
-const express = require('express');
 const router = express.Router();
-const Student = require('../models/Student');
 
 // Get all students
 router.get('/', async (req, res) => {
@@ -50,10 +50,11 @@ router.delete('/:id', async (req, res) => {
   try {
     const student = await Student.findOneAndDelete({ id: req.params.id });
     if (!student) return res.status(404).json({ message: 'Student not found' });
+
     res.json({ message: 'Student deleted' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
-module.exports = router;
+export default router;

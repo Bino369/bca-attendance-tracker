42aff62e3c2c954c3e48995bb62511bc71da09dc
Binoy Anil <100678709+Bino369@users.noreply.github.com>
2025-11-19 22:12:35 +0530

    Fix array syntax for student entries in constants.ts

--- constants.ts
+++ constants.ts
@@ -55,4 +55,4 @@ export const STUDENTS: Student[] = [
 { id: '44', name: 'UDISH B', rollNo: 44 },
 { id: '45', name: 'YOUHAN ABRAHAM GIFTY', rollNo: 45 },
 { id: '46', name: 'PRANAV. A. V', rollNo: 46 },
-{ id: '47', name: 'PRAJITH J', rollNo: 47 }
+{ id: '47', name: 'PRAJITH J', rollNo: 47 }]
